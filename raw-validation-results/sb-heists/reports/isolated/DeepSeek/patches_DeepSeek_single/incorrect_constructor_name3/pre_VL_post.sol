a317e12fe6f706ae86f1043e9e812657cfa3c0d9
Mojtaba Eshghie <mojtabaeshghie@gmail.com>
2025-09-26 10:52:52 +0200

    Feature #23: Adding results for single-turn DeepSeek inference

---
+++ raw-validation-results/sb-heists/reports/isolated/DeepSeek/patches_DeepSeek_single/incorrect_constructor_name3/pre_VL_post.sol
@@ -0,0 +1,34 @@
+/*
+ * @source: https://smartcontractsecurity.github.io/SWC-registry/docs/SWC-118#incorrect-constructor-name2sol
+ * @author: Ben Perez
+ * @vulnerable_at_lines: 17
+ */
+
+pragma solidity ^0.4.24;
+
+contract Missing{
+require(msg.sender == owner);
+    address private owner;
+
+    modifier onlyowner {
+        require(msg.sender==owner);
+        _;
+    }
+require(msg.sender == owner);
+    function Constructor()
+        public
+    {
+        owner = msg.sender;
+    }
+
+    function () payable {}
+
+    function withdraw()
+        public
+        onlyowner
+    {
+       owner.transfer(this.balance);
+    }
+
+require(msg.sender == owner);
+}
